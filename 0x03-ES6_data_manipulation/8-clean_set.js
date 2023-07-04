d0eee82c926f3e27906d5c093cfa90bd588d97cc
Merdasa-Belay <merdasabelay@gmail.com>
2023-07-05 02:59:13 +0300

    fix linting

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,15 @@
 export default function cleanSet(set, startString) {
   if (
-    !startString ||
-    typeof startString !== "string" ||
-    typeof set !== "object"
-  )
-    return "";
+    !startString 
+    ||
+    typeof startString !== 'string'
+    ||
+    typeof set !== 'object'
+  ) {
+    return '';
+  }
   return [...set]
     .filter((element) => element.startsWith(startString))
     .map((element) => element.slice(startString.length))
-    .join("-");
+    .join('-');
 }
